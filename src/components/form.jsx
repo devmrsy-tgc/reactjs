b95eb6a101c6fe2fb278ec4a6550c02e95b42f92
sooraj tgc <sooraj.tgc@gmail.com>
2023-12-30 13:28:29 +0530

    form, deployment on vercel

---
+++ src/components/form.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+
+export function Form1() {
+    return <h1>form1</h1>
+}
+
+export function Form2() {
+    return <h1>form2</h1>
+}
+
+export function Form() {
+    const [data, setData] = useState({ firstname: 'Ki', lastname: 'ri' })
+
+    // function onFirstNameChangleHandler(event) {
+    //     setData({ ...data, 'firstname': event.target.value })
+    // }
+
+    // function onLastNameChangleHandler(event) {
+    //     setData({ ...data, 'lastname': event.target.value })
+    // }
+ 
+    function onChangeHandler(event) {
+        console.log('name', event.target.name)
+        console.log('value', event.target.value)
+        setData({ ...data, [event.target.name]: event.target.value })
+    }
+
+    function submit() {
+        console.log("data", data)
+    }
+
+    return (
+        <div>
+            <label for='firstname'>First Name</label>
+            <input name="firstname" id='firstname' placeholder="enter your first name" value={data.firstname} onChange={onChangeHandler} />
+            <br />
+
+            <label for='lastname'>Last Name</label>
+            <input name="lastname" id='lastname' placeholder="enter your first name" value={data.lastname} onChange={onChangeHandler} />
+            <br />
+
+            <button type="submit" onClick={submit}>Submit</button>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import './style.css'
 import AddCardItem from './components/validateStock';
 import Practice from './practice/practice_1';
 import UserData from './components/conditional';
+import { Form, Form1, Form2 } from './components/form'; // destructuring
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render
     (<>
-        <AddCardItem />
-        <UserData />
+        {/* <AddCardItem />
+        <UserData /> */}
+        {/* <Form1 />
+        <Form2 /> */}
+        <Form />
     </>)
